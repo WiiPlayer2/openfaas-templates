cc3f8428fe6cce9da04edef163e025a1835d4995
Waldemar Tomme <waldetomme@gmail.com>
2022-03-04 13:59:23 +0100

    Update DarkLink.OpenFaaS to 0.0.3-dev

--- template/csharp-net6/function/Program.cs
+++ template/csharp-net6/function/Program.cs
@@ -1,7 +1,7 @@
 using DarkLink.OpenFaaS;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Configuration.AddOpenFaaSSecrets();
+builder.AddOpenFaaS();
 
 var app = builder.Build();
 app.MapFunction((ctx, arg) => $"Hello {arg} using {ctx.Request.Headers.UserAgent}!");
